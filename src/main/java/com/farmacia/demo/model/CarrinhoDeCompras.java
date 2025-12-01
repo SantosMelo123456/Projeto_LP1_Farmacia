79b75af3c3ad3487cbfc5eca3e9017cddbf131cf
LindaMary <lindamariasmelo@gmail.com>
2025-11-30 23:44:26 -0300

    Update CarrinhoDeCompras.java

--- src/main/java/com/farmacia/demo/model/CarrinhoDeCompras.java
+++ src/main/java/com/farmacia/demo/model/CarrinhoDeCompras.java
@@ -9,19 +9,19 @@ public class CarrinhoDeCompras {
     private List<Carrinho> itens;
     private FormaPagamento formaPagamento;
 
-    // Construtor
+    
     public CarrinhoDeCompras() {
         this.itens = new ArrayList<>();
     }
 
-    // 1. Adicionar Item
+    
     public void adicionarItem(Carrinho item) {
         if (item != null) {
             this.itens.add(item);
         }
     }
 
-    // 2. Calcular o Valor Total da compra automaticamente
+    
     public double calcularValorTotal() {
         double total = 0.0;
         for (Carrinho item : itens) {
@@ -30,12 +30,12 @@ public class CarrinhoDeCompras {
         return total;
     }
 
-    // 3. Escolher a Forma de Pagamento
+    
     public void escolherFormaPagamento(FormaPagamento formaPagamento) {
         this.formaPagamento = formaPagamento;
     }
 
-    // Getters
+    
     public List<Carrinho> getItens() {
         return itens;
     }
