e47197dd5d3727963e5971bf942c62c04ad9ccc5
LindaMary <lindamariasmelo@gmail.com>
2025-11-30 23:30:32 -0300

    comitei e puschei com Login!

--- src/main/java/com/farmacia/demo/model/Login.java
+++ src/main/java/com/farmacia/demo/model/Login.java
@@ -1,4 +1,38 @@
 package com.farmacia.demo.model;
 
-public class Login {
+public class Login { // <-- CORREÇÃO: ADICIONAMOS 'class'
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    // Construtor vazio (boas práticas)
+    public Login() {
+    }
+
+    // --- Getters e Setters (com corpos de método) ---
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
